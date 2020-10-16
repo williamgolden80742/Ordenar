4898093cac34e8667f110549ee47f123db7a246c
williamgolden80742 <52113843+williamgolden80742@users.noreply.github.com>
2020-10-16 13:39:10 -0300

    atualizando ordem de exibição

--- src/Ordenar/Home.java
+++ src/Ordenar/Home.java
@@ -5,7 +5,7 @@ class Home {
 
     public static void main(String[] args) throws IOException {
         long[] arr;
-        int pos = 5;
+        int pos = 6;
         String url = "C:\\Users\\William\\Desktop\\DadosGeoProcessamento2.csv";
         RadixSort r = new RadixSort();
         BubbleSort b = new BubbleSort();
@@ -13,20 +13,20 @@ class Home {
        
         arr = new long[172801];       
         r.values(url,arr, pos);         
-        r.runRadixSort(arr);
         System.out.println("\nRadix Sort : \n");
+        r.runRadixSort(arr);
         r.print(arr);
         
         arr = new long[172801];
         q.values(url,arr, pos);
+        System.out.println("\n\nQuick Sort : \n");        
         q.runQuickSort(arr);
-        System.out.println("\n\nQuick Sort : \n");
         q.print(arr);
         
         arr = new long[172801];
         b.values(url,arr, pos);
+        System.out.println("\n\nBubble Sort : \n");        
         b.runBubbleSort(arr);
-        System.out.println("\n\nBubble Sort : \n");
         b.print(arr);
 
     }
